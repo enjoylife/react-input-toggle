9f200483d182b3abbc39ae1553bb9040aeb28e25
Matt Clemens <matt.d.clemens@gmail.com>
2015-11-05 17:52:14 -0800

    add production check

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,11 @@ module.exports = {
     ]
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
     new webpack.NoErrorsPlugin(), new WebpackErrorNotificationPlugin(/* strategy */)
   ]
 }
